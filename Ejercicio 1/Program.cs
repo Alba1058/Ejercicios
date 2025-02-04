8d32ba498b77f509bd184d8c741574433df475bd
alba1058 <albathen386@gmail.com>
2025-02-03 22:11:36 -0400

    Initial commit

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Math
+{
+    static void Main()
+    {
+        float a, b, c, x;
+        Console.WriteLine("Ingrese el valor de a:");
+        a = float.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el valor de b:");
+        b = float.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el valor de c:");
+        c = float.Parse(Console.ReadLine());
+        x = (a + (b / c)) / ((a / b) + c);
+        Console.WriteLine($"X es igual a: {x}");
+        Console.ReadLine();
+    }
+
+}
+
+
+   
+
+
